4a360d8a132ff2074fe17adbcca6c2ba488f9536
soarkey-PC <985686485@qq.com>
2020-06-18 13:29:24 +0800

    Site updated: 2020-06-18 13:29:17

--- service-worker.js
+++ service-worker.js
@@ -285,7 +285,7 @@ self.addEventListener('fetch', function(event) {
 
 // Runtime cache configuration, using the sw-toolbox library.
 
-toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"https://soarkey.github.io/"});
+toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"soarkey.github.io"});
 
 
 
